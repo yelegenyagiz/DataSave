bcf538892aa7331fe3d09725932cc81f178f2929
Yağız <yelegenyagiz@gmail.com>
2023-03-19 16:37:33 +0300

    some editting.

--- Assets/Handler.cs
+++ Assets/Handler.cs
@@ -10,6 +10,16 @@ public class Handler : MonoBehaviour
     private void Start()
     {
         SaveLoad.Write<int>("My age", 24);
+        SaveLoad.Write<Vector3>("position", new Vector3(20, 30, 40));
+        SaveLoad.Write<bool>("Am i a good person?", false);
+        SaveLoad.Write<string>("My surname", "YELEGEN");
+        SaveLoad.Write<float>("pi", 3.1415f);
+
+        var v1 = SaveLoad.Read<int>("My age");
+        var v2 = SaveLoad.Read<Vector3>("position");
+        var v3 = SaveLoad.Read<System.Boolean>("Am i good person?");
+        var v4 = SaveLoad.Read("My surname");
+
     }
 
 }
